61b554c387e8fde308f29e06b7ea5a2efade0e53
VladAvdeev <vladislav.avdeev55@gmail.com>
2022-10-10 22:59:28 +0300

    Добавьте файлы проекта.

---
+++ MineServer/MyServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MineServer
+{
+    public static class MyServer
+    {
+        
+            static int count;
+            static ReaderWriterLock readerLock = new ReaderWriterLock();
+            public static int GetCount()
+            {
+                readerLock.AcquireReaderLock(Timeout.InfiniteTimeSpan);
+            try
+            {
+                return count;
+            }
+            finally 
+            { 
+                readerLock.ReleaseReaderLock(); 
+            }
+
+            }
+            public static void AddToCount(int value)
+            {
+                readerLock.AcquireWriterLock(Timeout.InfiniteTimeSpan);
+                count += value;
+                readerLock.ReleaseWriterLock();
+            }
+
+        
+    }
+}
---
+++ MineServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MineServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Parallel.For(0, 100, x =>
+                {
+                    if (x % 2 == 0)
+                        MyServer.AddToCount(2);
+                    else
+                        Console.WriteLine(MyServer.GetCount());
+                });
+            Console.WriteLine(MyServer.GetCount());
+            Console.ReadLine();
+        }
+    }
+}
